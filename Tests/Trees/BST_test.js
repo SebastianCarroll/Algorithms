90c66072645c67fc5103765f1c9ae4343b360fbe
Sebastian Carroll <sjc@localhost.localdomain>
2013-11-15 23:35:35 +1100

    Added search, min, max and successor methods to the BST. Working on predecessor

--- Tests/Trees/BST_test.js
+++ Tests/Trees/BST_test.js
@@ -1,21 +1,118 @@
 module("Testing Insertion");
 test("Initially empty tree", function() {
     var bst = new BST();
-    bst.insert(new Node(null, 10));
+    bst.insert(10);
     equal(10, bst.root.value);
 });
 
 test("Only root, node < root", function() {
     var bst = new BST();
-    bst.insert(new Node(null, 10));
-    bst.insert(new Node(null, 9));
+    bst.insert(10);
+    bst.insert(9);
     equal(9, bst.root.left.value);
 });
 
 test("Only root, node > root", function() {
     var bst = new BST();
-    bst.insert(new Node(null, 10));
-    bst.insert(new Node(null, 11));
+    bst.insert(10);
+    bst.insert(11);
     equal(11, bst.root.right.value);
 });
 
+module("Testing Search");
+test("Empty tree", function() {
+    var bst = new BST();
+    var found = bst.search(10);
+    equal(found, null);
+});
+
+test("Only root", function() {
+    var bst = new BST();
+    bst.insert(10);
+    var found = bst.search(10);
+    equal(found.value, 10);
+});
+
+test("Large Tree - leaf", function() {
+    var bst = new BST();
+    bst.insert(10);
+    bst.insert(1);
+    bst.insert(100);
+    bst.insert(30);
+    bst.insert(12);
+    bst.insert(11);
+    var found = bst.search(11);
+    equal(found.value, 11);
+});
+
+module("Testing Min");
+test("Large Tree - leaf", function() {
+    var bst = new BST();
+    bst.insert(10);
+    bst.insert(1);
+    bst.insert(100);
+    bst.insert(30);
+    bst.insert(12);
+    bst.insert(11);
+    var min = bst.min();
+    equal(min.value, 1);
+});
+
+test("Empty Tree", function() {
+    var bst = new BST();
+    var min = bst.min();
+    equal(min.value, null);
+});
+
+module("Testing Max");
+test("Large Tree", function() {
+    var bst = new BST();
+    bst.insert(10);
+    bst.insert(1);
+    bst.insert(100);
+    bst.insert(30);
+    bst.insert(12);
+    bst.insert(11);
+    var max = bst.max();
+    equal(max.value, 100);
+});
+
+test("Empty Tree", function() {
+    var bst = new BST();
+    var max = bst.max();
+    equal(max.value, null);
+});
+
+module("Testing Successor");
+test("Large Tree", function() {
+    var bst = new BST();
+    bst.insert(10);
+    bst.insert(1);
+    bst.insert(100);
+    bst.insert(30);
+    bst.insert(12);
+    bst.insert(11);
+    var found = bst.search(10);
+    var next = bst.successor(found);
+    equal(next.value, 11);
+});
+
+test("Empty Tree", function() {
+    var bst = new BST();
+    var next = bst.successor(new Node(null, 10));
+    equal(next, null);
+});
+
+test("Large Not Root", function() {
+    var bst = new BST();
+    bst.insert(10);
+    bst.insert(1);
+    bst.insert(30);
+    bst.insert(100);
+    bst.insert(12);
+    bst.insert(11);
+    bst.insert(9);
+    var found = bst.search(9);
+    var next = bst.successor(found);
+    equal(next.value, 10);
+});
--- Trees/BST.js
+++ Trees/BST.js
@@ -10,35 +10,73 @@ function BST(){
     
     var root = new Node();
     
-    function insert(toIns, node){
+    function insert(value, node){
         node = node || root;
         if(node.value === null){
-            node.value = toIns.value;
+            node.value = value;
             node.left  = new Node(node);
             node.right = new Node(node);
-        } else if(toIns.value > node.value){
-           insert(toIns, node.right);
-        } else if(toIns.value < node.value){
-           insert(toIns, node.left);
+        } else if(value > node.value){
+           insert(value, node.right);
+        } else if(value < node.value){
+           insert(value, node.left);
         }
     }
     
-    function search(toFind, node){
+    function search(value, node){
         node = node || root;
         if(node.value === null){
             return null;
-        } else if(node.value === toFind){
+        } else if(node.value === value){
             return node;
-        } else if(toFind > node.value){
-           search(toFind, node.right);
-        } else if(toFind < node.value){
-           search(toFind, node.left);
+        } else if(value > node.value){
+           return search(value, node.right);
+        } else if(value < node.value){
+           return search(value, node.left);
         }
+    }
+    
+    function min(node){
+        node = node || root;
+        while (node && node.left && node.left.value !== null){
+            node = node.left;
+        }
+        return node;
+    }
+
+    
+    function max(node){
+        node = node || root;
+        while (node && node.right && node.right.value !== null){
+            node = node.right;
+        }
+        return node;
+    }
+    
+    function successor(node){
+        if(node && node.right && node.right.value !== null){
+            return min(node.right);
+        }
+        var parent = node.parent;
+        while(parent !== null && node === parent.right){
+            node = parent;
+            parent = parent.parent;
+        }
+        return parent;
+    }
+    
+    function predecessor(node){
+        
     }
     
     return {
         root: root,
         insert: insert,
-        search: search
+        search: search,
+        min: min,
+        max: max,
+        successor: successor
     };
 }
+
+
